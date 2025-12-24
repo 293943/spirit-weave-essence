1cc073de158b9d0a77255cc1d1e0bf40155e98f7
C <80237370+C293943@users.noreply.github.com>
2025-12-24 18:54:06 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/spirit-weave-essence/",
+  base: "./",
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/main.tsx"),
